fffae94ac52efd4fb37e37b55902ce15101972e2
kevin <kevinochieng548@gmail.com>
2017-12-12 14:48:41 +0300

    validate name presence

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -4,4 +4,6 @@ RSpec.describe Item, type: :model do
   #ensure item belongs to a todo
   it {should belong_to(:todo)}
 
+  #ensure column name is present
+  it { should validate_presence_of(:name) }
 end
