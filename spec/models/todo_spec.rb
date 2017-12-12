323ddb45d40d0b996ee207fea5166288695a155c
kevin <kevinochieng548@gmail.com>
2017-12-12 13:58:04 +0300

    generate presence of title and created_by

--- spec/models/todo_spec.rb
+++ spec/models/todo_spec.rb
@@ -4,4 +4,8 @@ RSpec.describe Todo, type: :model do
   # pending "add some examples to (or delete) #{__FILE__}"
   #ensure Todo model has one to many relationships
   it { should have_many(:items).dependent(:destroy) }
+
+  #test for title and created_by presence
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:created_at) }
 end
