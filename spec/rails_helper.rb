bb82914a11fb15ff99c218f71bf49e4bd5fe1b0a
kevin <kevinochieng548@gmail.com>
2017-12-12 13:13:53 +0300

    trucate all tables

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -37,13 +37,17 @@ end
 
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
-
+  #add FactoryGirl module
+  config.include FactoryGirl::Syntax::Methods
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = true
 
+  #truncating all tables
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+    DatabaseCleaner.stategy = :transaction
+  end
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
   # `post` in specs under `spec/controllers`.
