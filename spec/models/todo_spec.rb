3021a6faa53206c6c3bd2945224282aa50e330b4
kevin <kevinochieng548@gmail.com>
2017-12-12 13:56:12 +0300

    test one to many association

--- spec/models/todo_spec.rb
+++ spec/models/todo_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Todo, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # pending "add some examples to (or delete) #{__FILE__}"
+  #ensure Todo model has one to many relationships
+  it { should have_many(:items).dependent(:destroy) }
 end
