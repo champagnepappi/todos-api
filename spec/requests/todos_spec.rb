ce460a24d163c2e668c3b20516be97e2f41092ac
kevin <kevinochieng548@gmail.com>
2017-12-19 15:08:01 +0300

    make http request before each example

--- spec/requests/todos_spec.rb
+++ spec/requests/todos_spec.rb
@@ -4,4 +4,9 @@ RSpec.describe 'Todos API', type: :request do
   #initialize test data
   let!(:todo) {create_list(:todo, 10)}
   let(:todo_id) {todos.first.id}
+
+  #Test suite for GET /todos
+  describe 'GET /todos' do
+    before { get '/todos' }
+  end
 end
