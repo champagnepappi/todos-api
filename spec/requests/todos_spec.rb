24dcad9026dcf7c6313da820f77aef73df63cec4
kevin <kevinochieng548@gmail.com>
2018-01-11 15:32:51 +0300

    return status code

--- spec/requests/todos_spec.rb
+++ spec/requests/todos_spec.rb
@@ -13,5 +13,9 @@ RSpec.describe 'Todos API', type: :request do
       expect(json).not_to be_empty
       expect(json.size).to eq(10)
     end
+
+    it 'returns status code 200' do
+      expect(response).to have_http_status(200)
+    end
   end
 end
