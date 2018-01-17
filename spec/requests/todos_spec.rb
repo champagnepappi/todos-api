6acfe889648fa3fd34678b98e59acdb5f3893a8b
kevin <kevinochieng548@gmail.com>
2018-01-17 11:40:00 +0300

    modify initialized test data

--- spec/requests/todos_spec.rb
+++ spec/requests/todos_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'Todos API', type: :request do
   #initialize test data
-  let!(:todo) {create_list(:todo, 10)}
+  let!(:todos) {create_list(:todo, 10)}
   let(:todo_id) {todos.first.id}
 
   #Test suite for GET /todos
