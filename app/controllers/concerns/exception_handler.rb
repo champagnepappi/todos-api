ed60790b94dd53702a52503356e3684d3aaaf5e0
kevin <kevinochieng548@gmail.com>
2018-01-17 11:18:23 +0300

    rescue the record not found exception

--- app/controllers/concerns/exception_handler.rb
+++ app/controllers/concerns/exception_handler.rb
@@ -2,4 +2,9 @@ module ExceptionHandler
   #provides the more graceful 'included' method
   extend ActiveSupport::Concern
 
+  included do
+    rescue_from ActiveRecord::RecordNotFound do |e|
+      json_response({message: e.message}, :not_found)
+    end
+  end
 end
