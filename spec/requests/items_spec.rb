9fe445b1af5c0bc99cc703bcd8210f767b34d558
Kevin Santos <kevinochieng548@gmail.com>
2018-09-08 13:08:33 +0300

    --amend
    initialize test data

--- spec/requests/items_spec.rb
+++ spec/requests/items_spec.rb
@@ -5,5 +5,5 @@ RSpec.describe 'Items API' do
   let!(:todo) {create(:todo)}
   let!(:items) {create_list(:item, 20, todo_id: todo.id)}
   let!(:todo_id) {todo.id}
-  let(id) {items.first.id}
+  let(:id) {items.first.id}
 end
