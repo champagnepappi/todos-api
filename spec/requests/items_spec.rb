1b666932eb46156a92c21b930452482cfd814b8f
Kevin Santos <kevinochieng548@gmail.com>
2018-09-08 13:01:16 +0300

    initialize test data

--- spec/requests/items_spec.rb
+++ spec/requests/items_spec.rb
@@ -1,1 +1,9 @@
 require 'rails_helper'
+
+RSpec.describe 'Items API' do
+  #initialize the test data
+  let!(:todo) {create(:todo)}
+  let!(:items) {create_list(:item, 20, todo_id: todo.id)}
+  let!(:todo_id) {todo.id}
+  let(id) {items.first.id}
+end
