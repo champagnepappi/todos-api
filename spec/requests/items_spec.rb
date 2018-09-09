08f2f60e41ef9794822fe790f1fb16a821faa1a2
Kevin Santos <kevinochieng548@gmail.com>
2018-09-09 13:59:14 +0300

    test a not found message if item does not exist

--- spec/requests/items_spec.rb
+++ spec/requests/items_spec.rb
@@ -106,6 +106,10 @@ RSpec.describe 'Items API' do
       it 'returns status code 404' do
         expect(response).to have_http_status(404)
       end
+
+      it 'returns a not found message' do
+        expect(response).to match(/Couldn't find Item/)
+      end
     end
   end
 end
