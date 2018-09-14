c6c863fbea56a6ddb4bbafeeed54ce1de8eb882a
Kevin Santos <kevinochieng548@gmail.com>
2018-09-14 14:25:53 +0300

    use validates presence of

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ApplicationRecord
   has_many :todos, foreign_key: :created_by
 
   has_secure_password
-  validates :name, :email, :password_digest
+  validates_presence_of :name, :email, :password_digest
 end
