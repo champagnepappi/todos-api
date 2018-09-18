6f5dfb1e8e15225cce77c29cf57aa32e6f68c744
Kevin Santos <kevinochieng548@gmail.com>
2018-09-18 22:24:07 +0300

    test raise exception when token expires

--- spec/auth/authorize_api_request_spec.rb
+++ spec/auth/authorize_api_request_spec.rb
@@ -35,6 +35,16 @@ RSpec.describe AuthorizeAPiRequest do
         raise_error(ExceptionHandler::InvalidToken, /Invalid Token/)
       end
     end
+
+    context 'when token is expired' do
+      let(:header) {{'Authorization' => expired_token_generator(user.id)}}
+      subject(:request_obj) {described_class.new(header)}
+
+      it 'raises ExceptionHandler::ExpiredSignature error' do
+        expect {request_obj.call}
+          .to raise_error(ExceptionHandler::InvalidToken, /Signature has expired/)
+      end 
+    end
   end
 
 end
