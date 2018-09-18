c66621609d75f6ed8db6eadcc9f8a982ab96ec5a
Kevin Santos <kevinochieng548@gmail.com>
2018-09-18 20:39:54 +0300

    define json response for status code 422

--- app/lib/exception_handler.rb
+++ app/lib/exception_handler.rb
@@ -13,8 +13,14 @@ module ExceptionHandler
     rescue_from ExceptionHandler::MissingToken, with: :fourty_twenty_two
     rescue_from ExceptionHandler::InvalidToken, with: :fourty_twenty_two
 
-  rescue_from ActiveRecord::RecordNotFound do |e|
-    json_response({message: e.message}, :not_found)
+    rescue_from ActiveRecord::RecordNotFound do |e|
+      json_response({message: e.message}, :not_found)
+    end
   end
+
+  private
+  #JSON response with message; Status code 422 - unprocessable entity
+  def four_twenty_two(e)
+    json_response({message: e.message}, :unprocessable_entity)
   end
 end
