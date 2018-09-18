85e657343387993deae9531ef71ad188a0c7e509
Kevin Santos <kevinochieng548@gmail.com>
2018-09-18 20:36:38 +0300

    add custom handler invalid token

--- app/lib/exception_handler.rb
+++ app/lib/exception_handler.rb
@@ -12,5 +12,9 @@ module ExceptionHandler
     rescue_from ExceptionHandler::AuthenticationError, with: :unauthorised_request
     rescue_from ExceptionHandler::MissingToken, with: :fourty_twenty_two
     rescue_from ExceptionHandler::InvalidToken, with: :fourty_twenty_two
+
+  rescue_from ActiveRecord::RecordNotFound do |e|
+    json_response({message: e.message}, :not_found)
+  end
   end
 end
