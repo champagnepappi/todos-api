59c8a6964e756be0494bdfbe87816b6a14328765
Kevin Santos <kevinochieng548@gmail.com>
2018-09-18 11:55:14 +0300

    setup a valid and invalid request onject

--- spec/auth/authorize_api_request_spec.rb
+++ spec/auth/authorize_api_request_spec.rb
@@ -5,5 +5,7 @@ RSpec.describe AuthorizeAPiRequest do
   let(:user) {create(:user)}
   #Mock 'athorization header'
   let(:header) {{'Authorization' => token_generator(:user.id)}}
+  subject(:invalid_request_obj) { described_class.new({}) }
+  subject(:request_job) {described_class.new(header)}
 
 end
