7d47269b11bafe3d52721e8edb3a11b8e95a3b3c
Kevin Santos <kevinochieng548@gmail.com>
2018-09-18 12:00:31 +0300

    return user object when request is valid

--- spec/auth/authorize_api_request_spec.rb
+++ spec/auth/authorize_api_request_spec.rb
@@ -8,4 +8,13 @@ RSpec.describe AuthorizeAPiRequest do
   subject(:invalid_request_obj) { described_class.new({}) }
   subject(:request_job) {described_class.new(header)}
 
+  describe '#call' do
+    context 'when valid request' do
+      it 'returns user object' do
+        result = request_obj.call
+        expect(result[:user]).to eq(user)
+      end
+    end
+  end
+
 end
