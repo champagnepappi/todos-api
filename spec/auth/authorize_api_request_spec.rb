5f6866ceee4867bb0e65f5094ab22ac21acfa7f2
Kevin Santos <kevinochieng548@gmail.com>
2018-09-18 10:43:02 +0300

    mock authorization header

--- spec/auth/authorize_api_request_spec.rb
+++ spec/auth/authorize_api_request_spec.rb
@@ -3,4 +3,7 @@ require 'rails_helper'
 RSpec.describe AuthorizeAPiRequest do
   #create test user
   let(:user) {create(:user)}
+  #Mock 'athorization header'
+  let(:header) {{'Authorization' => token_generator(:user.id)}}
+
 end
