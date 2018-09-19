0725372ee7b8560c4887cdf593cb10abebaf0f53
Kevin Santos <kevinochieng548@gmail.com>
2018-09-19 15:00:31 +0300

    memoize user object and rescue exception

--- app/auth/authorize_api_request.rb
+++ app/auth/authorize_api_request.rb
@@ -8,4 +8,14 @@ class AuthorizeApiRequest
       user: user
     }
   end
+
+  private
+
+  attr_reader :headers
+
+  def user
+    @user ||= User.find(decoded_auth_token[:user_id]) if decoded_auth_token
+  rescue ActiveRecord::RecordNotFound => e
+    raise(ExceptionHandler::InvalidToken,("#{Message.invalid_token} #{e.message}") )
+  end
 end
