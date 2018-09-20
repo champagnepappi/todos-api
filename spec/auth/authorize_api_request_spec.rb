2c3a6ef64e754a11ab1df677b203252bd6c49c6c
Kevin Santos <kevinochieng548@gmail.com>
2018-09-20 12:42:38 +0300

    modify the spec to fix test failures

--- spec/auth/authorize_api_request_spec.rb
+++ spec/auth/authorize_api_request_spec.rb
@@ -19,8 +19,8 @@ RSpec.describe AuthorizeApiRequest do
     context 'when invalid request' do
       context 'when missing token' do
         it 'raises a MissingToken error' do
-          expect {invalid_request_obj.call}.to
-          raise_error(ExceptionHandler::MissingToken, 'MIssing token')
+          expect {invalid_request_obj.call}
+            .to raise_error(ExceptionHandler::MissingToken, 'Missing token')
         end
       end
     end
@@ -31,8 +31,7 @@ RSpec.describe AuthorizeApiRequest do
       end
 
       it 'raises an Invalid Token error' do
-        expect {invalid_request_obj.call}.to 
-        raise_error(ExceptionHandler::InvalidToken, /Invalid Token/)
+        expect {invalid_request_obj.call}.to raise_error(ExceptionHandler::InvalidToken, /Invalid token/)
       end
     end
 
@@ -41,8 +40,7 @@ RSpec.describe AuthorizeApiRequest do
       subject(:request_obj) {described_class.new(header)}
 
       it 'raises ExceptionHandler::ExpiredSignature error' do
-        expect {request_obj.call}
-          .to raise_error(ExceptionHandler::InvalidToken, /Signature has expired/)
+        expect {request_obj.call}.to raise_error(ExceptionHandler::InvalidToken, /Signature has expired/)
       end 
     end
 
@@ -50,8 +48,7 @@ RSpec.describe AuthorizeApiRequest do
       let(:header) {{'Authorization' => 'foobar'}}
       subject(:invalid_request_obj) {described_class.new(header)}
       it 'handles JWT::DecodeError' do
-        expect {invalid_request_obj.call}.to 
-        raise_error(ExceptionHandler::InvalidToken, /Not enough or too many segments/)
+        expect {invalid_request_obj.call}.to raise_error(ExceptionHandler::InvalidToken, /Not enough or too many segments/)
       end
     end
   end
