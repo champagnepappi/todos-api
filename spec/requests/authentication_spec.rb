fc9e7a3fc8d99878e6c17feacb39f56b8782582d
Kevin Santos <kevinochieng548@gmail.com>
2018-09-24 17:44:28 +0300

    test failure message for invalid request

--- spec/requests/authentication_spec.rb
+++ spec/requests/authentication_spec.rb
@@ -25,5 +25,13 @@ RSpec.describe 'Authentication', type: :request do
         expect(json['auth_token']).not_to be_nil
       end
     end
+
+    context 'when request is invalid' do
+      before {post '/auth/login', params: invalid_credentials, headers: headers}
+
+      it 'returns a failure message' do
+        expect(json['message']).to match(/Invalid credential/)
+      end
+    end
   end
 end
