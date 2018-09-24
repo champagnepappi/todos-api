d217229322c8e4ac38ab28b661f8e964637e940c
Kevin Santos <kevinochieng548@gmail.com>
2018-09-24 17:26:18 +0300

    set headers for authorization

--- spec/requests/authentication_spec.rb
+++ spec/requests/authentication_spec.rb
@@ -1,7 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe 'Authentication', type: :request do
-  describe 'POST auth/login' do
+  describe 'POST /auth/login' do
     let(:user) {create(:user)}
+    let(:headers) {valid_headers.except('Authorization')}
   end
 end
