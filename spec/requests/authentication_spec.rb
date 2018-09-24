135377755ac9aff73598bd8cbcf920e728e0c369
Kevin Santos <kevinochieng548@gmail.com>
2018-09-24 17:32:39 +0300

    set test valid and invalid credentials

--- spec/requests/authentication_spec.rb
+++ spec/requests/authentication_spec.rb
@@ -4,5 +4,18 @@ RSpec.describe 'Authentication', type: :request do
   describe 'POST /auth/login' do
     let(:user) {create(:user)}
     let(:headers) {valid_headers.except('Authorization')}
+    let(:valid_credentials) do
+      {
+        email: user.email,
+        password: user.password
+      }.to_json
+    end
+
+    let(:invalid_credentials) do
+      {
+        email: Faker::Internet.email,
+        password: Faker::Internet.password
+      }
+    end
   end
 end
