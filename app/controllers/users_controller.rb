4ca2c7e974e74570f1e90708e1f268ea8e337d7f
Kevin Santos <kevinochieng548@gmail.com>
2018-09-29 17:00:03 +0300

    define users controller create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,12 @@
 class UsersController < ApplicationController
+
+  def create
+    user = User.create!(user_params)
+    auth_token = AuthenticationUser.new(user.email, user.password)
+    response = {message: Message.account_created, auth_token: auth_token}
+    json_response(response, :created)
+  end
+
   private
   def user_params
     params.permit(:name, :email, :password, :password_confirmation)
