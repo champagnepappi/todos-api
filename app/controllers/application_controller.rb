e4a25c4656a3efba96e22f45e747cb30ef5181e4
Kevin Santos <kevinochieng548@gmail.com>
2018-09-30 13:03:56 +0300

    check for valid request token and return user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
 class ApplicationController < ActionController::API
   include Response
   include ExceptionHandler
+
+  private
+  def authorize_request
+    @current_user = (AuthorizeApiRequest.new(request.headers).call)[:user]
+  end
 end
