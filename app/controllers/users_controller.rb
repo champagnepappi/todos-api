a4db2881d1999fecbffc69ed2e44873a591299bc
Kevin Santos <kevinochieng548@gmail.com>
2018-09-30 13:20:11 +0300

    modify user controller create action to fix test failures

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
 
   def create
     user = User.create!(user_params)
-    auth_token = AuthenticationUser.new(user.email, user.password)
+    auth_token = AuthenticateUser.new(user.email, user.password)
     response = {message: Message.account_created, auth_token: auth_token}
     json_response(response, :created)
   end
