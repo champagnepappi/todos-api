ad2267357b64a9bcc70b5efddc11d42f3d9de035
Kevin Santos <kevinochieng548@gmail.com>
2018-09-30 18:07:11 +0300

    fix error on test

--- spec/requests/todos_spec.rb
+++ spec/requests/todos_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe 'Todos API', type: :request do
   #initialize test data
   let(:user) {create(:user)}
-  let!(:todos) {create_list(:todo, 10, created_by: user.id}
+  let!(:todos) {create_list(:todo, 10, created_by: user.id)}
   let(:todo_id) {todos.first.id}
   #authorize request
   let(:headers) { valid_headers  }
