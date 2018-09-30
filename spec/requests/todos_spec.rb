1f713cf59dd3abe999b2aaaa86cc2f5a753990e4
Kevin Santos <kevinochieng548@gmail.com>
2018-09-30 16:17:54 +0300

    authorize request with valid headers

--- spec/requests/todos_spec.rb
+++ spec/requests/todos_spec.rb
@@ -5,6 +5,8 @@ RSpec.describe 'Todos API', type: :request do
   let(:user) {create(:user)}
   let!(:todos) {create_list(:todo, 10, created_by: user.id}
   let(:todo_id) {todos.first.id}
+  #authorize request
+  let(:headers) { valid_headers  }
 
   #Test suite for GET /todos
   describe 'GET /todos' do
